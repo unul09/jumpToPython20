9ea31204464a114370008bd221137ffd3d0169d6
unul09 <morning20404@gmail.com>
2022-03-03 18:02:13 +0900

    15

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# 14
-a = str(input("압축할 문자열 입력 : ")) + ' '
-print(a)
-b = ''
-count = 1
-for i in range(len(a) - 1):
-    if a[i] == a[i + 1]:
-        count += 1
-    else:
-        b += a[i] + str(count)
-        count = 1
-print(b)
+# 15
+data_bunch = input().split()
+for data in data_bunch:
+    nums = []
+    for s in data:
+        nums.append(int(s))
+    nums.sort()
+    right_data = [0,1,2,3,4,5,6,7,8,9]
+    print(nums == right_data, end=' ')
