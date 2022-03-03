0140fd80e3daa04ecabf92409142c92cf2cf71e6
unul09 <morning20404@gmail.com>
2022-03-03 18:16:08 +0900

    16

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-# 15
-data_bunch = input().split()
-for data in data_bunch:
-    nums = []
-    for s in data:
-        nums.append(int(s))
-    nums.sort()
-    right_data = [0,1,2,3,4,5,6,7,8,9]
-    print(nums == right_data, end=' ')
+# 16
+dic = {
+    '.-':'A','-...':'B','-.-.':'C','-..':'D','.':'E','..-.':'F',
+    '--.':'G','....':'H','..':'I','.---':'J','-.-':'K','.-..':'L',
+    '--':'M','-.':'N','---':'O','.--.':'P','--.-':'Q','.-.':'R',
+    '...':'S','-':'T','..-':'U','...-':'V','.--':'W','-..-':'X',
+    '-.--':'Y','--..':'Z'
+}
+
+mos = input()
+result = ''
+for i in mos.split('  '):
+    for j in i.split(' '):
+        result += dic[j]
+    result += ' '
+
+print(result)
