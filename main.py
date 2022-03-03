39a7c1df32727927d9a21909690cfcad042c0e74
unul09 <morning20404@gmail.com>
2022-03-03 12:00:37 +0900

    6

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# 5
-n = int(input())
-
-fibo1 = 0
-fibo2 = 1
-print(fibo1, end=' ')
-
-while fibo2 <= n:
-    print(fibo2, end=' ')
-    temp = fibo1
-    fibo1 = fibo2
-    fibo2 = fibo2 + temp
+# 6
+A = map(int, input().split(','))
+print(sum(A))
