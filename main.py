2b2c504830036344d4ed08fbd787856d8984e2c4
unul09 <morning20404@gmail.com>
2022-03-03 11:59:54 +0900

    4

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# 2
-try:
-    a = {'A': 90, 'B': 80}
-    print(a['C'])
-
-except KeyError:
-    a['C'] = 70
-    print(a['C'])
+# 4
+# 3번은 코드작성문제가 아니라 제외했음
+A = [20, 55, 67, 82, 45, 33, 90, 87, 100, 25]
+Asum = sum(filter(lambda x: x > 50, A))
+print(Asum)
